b472fea23d06a7ccca11d78075cab37acf43b1d0
thelfer1 <thomashelfer@live.de>
2023-12-29 13:08:03 -0500

    improved requirements and setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
-from setuptools import setup
-from setuptools import find_packages
+from setuptools import setup, find_packages
 
 
 def readme():
@@ -7,24 +6,29 @@ def readme():
         return f.read()
 
 
+def read_requirements():
+    with open("requirements.txt") as f:
+        return f.read().splitlines()
+
+
 setup(
     name="GeneralRelativity",
     version="0.1",
-    description="HBA with GPR and PCA",
+    description="A translation of crucial parts of GRTL in torch for accelerated learning",
     long_description=readme(),
     classifiers=[
         "Development Status :: 3 - Alpha",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3.8",
         "Topic :: Machine learning :: Physics :: Simulation :: General Relativity",
     ],
     keywords="Machine learning, Physics, Simulation, General Relativity",
     author="ThomasHelfer",
     author_email="thomashelfer@live.de",
-    license="MIT",
+    license="Apache 2.0",
     packages=find_packages(exclude=["tests"]),
-    install_requires=["torch", "black", "pre-commit", "pytest", "numpy"],
-    python_requires=">=3.5 ",
+    install_requires=read_requirements(),
+    python_requires=">=3.8",
     include_package_data=True,
     zip_safe=False,
 )
