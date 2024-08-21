5b41a200066c35a831b6fceebcc4086b3e838842
thelfer1 <thomashelfer@live.de>
2024-08-21 06:17:56 -0400

    improved evaluate models

--- evaluate_models.py
+++ evaluate_models.py
@@ -2,9 +2,17 @@ import sys
 import os
 import argparse
 import pandas as pd
+import torch
 
 from SuperResolution.models import calculate_test_loss, load_model, check_performance
 from SuperResolution.utils import load_config, copy_config_file
+
+
+def clean_tensor_values(value):
+    if isinstance(value, torch.Tensor):
+        return value.item()
+    return value
+
 
 metrics = []
 for name in os.listdir("models"):
@@ -15,13 +23,13 @@ for name in os.listdir("models"):
         L2Hmodelerror, L2Hinterp = check_performance(
             net=net,
             config=config,
-            datafolder=f"/home/thelfer1/scr4_tedwar42/thelfer1/high_end_data_{i}/outputXdata_level{config['res_level']}_step*.dat",
+            datafolder=f"/home/thelfer1/scr4_tedwar42/thelfer1/high_end_data_{i}/outputXdata_level{config['res_level']}_step00[04]*.dat",
         )
         factor = L2Hinterp / L2Hmodelerror
         metric[f"factor{i}"] = factor
         metric[f"L2Hinterp{i}"] = L2Hinterp
         metric[f"L2Hmodelerror{i}"] = L2Hmodelerror
     metrics.append(metric)
-
-df = pd.DataFrame(metrics)
-df.to_csv("metrics_results.csv", index=False)
+    df = pd.DataFrame(metrics)
+    df = df.applymap(clean_tensor_values)
+    df.to_csv("metrics_results.csv", index=False)
